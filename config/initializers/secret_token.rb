119a9b80eca39f5961aa77d0c1d4e8815217f1e8
Neeraj Singh <neerajdotname@gmail.com>
2011-08-03 16:28:26 -0400

    done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneTodosEbookApp::Application.config.secret_token = '03a77ba131017c6fbd16580955c6094a2bb22cd4bcebac3b2aca54feb83f9d49be9b000e9e974b5de71fcf25067560535795f5998fb059f0563c5a2c7b87ad5f'
